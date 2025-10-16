68f3cd054fd6b09159fa98f9f9240ccd6c199663
yurij <mihov.yurij@gmail.com>
2025-10-16 11:50:49 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,30 @@
+
+function getRandomInt(min, max) {
+    const range = max - min + 1
+    return Math.floor(Math.random() * range + min);
+}
+
+console.log(getRandomInt(1, 10));
+console.log(getRandomInt(20, 80));
+console.log(getRandomInt(1, 100));
+
+
+
+
+function greet(message, names) {
+    console.log(`${message}, ${names}`);
+    return String(message), String(names);
+};
+greet('Hey', 'Bob');
+greet('Hello' , 'Mary');
+greet('Hi' , 'John');
+
+
+
+function sumBigIntegers(numStr1, numStr2) {
+    return Number(numStr1) + Number(numStr2);
+}
+let numStr1 = '9007199254740991';
+let numStr2 = '9007199254740991';
+const result = sumBigIntegers(numStr1, numStr2);
+console.log(result);
